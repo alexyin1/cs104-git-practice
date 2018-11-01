3e5dcf4259ad6e799e99b06e4ee5059fc413d12e
Alex Yin <alexyin@usc.edu>
2018-11-01 12:14:57 -0700

    updated

---
+++ hw6/coloring.cpp
@@ -0,0 +1,154 @@
+#include <queue> 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#define ncolors = 4;
+using namespace std;
+
+//keep track of all indexes belonging to country
+//keep track of all bordering countries
+
+struct Country{
+    int color;
+    char letter;
+    set<char> neighbors;
+    Country(char c, int col){
+        letter = c;
+        color = col; //0 as no color
+    }
+};
+
+void printSolution(vector<Country>& countries){
+    char name;
+    for(size_t i=0; i<countries.size(); i++){
+        name = 'A'+ i;
+        cout << name << ' ' << countries[i].color;
+        cout <<endl;
+    }
+
+     cout <<endl;
+}
+
+bool colorsConsistant(int row, vector<Country>& countries){
+    //iterate through set instead
+        for(char c : countries[row].neighbors) {
+            if(countries[row].color == countries[(int)c-65].color && countries[row].color!=0){  //if true means a neighbor shares colors
+                return 0; 
+            }
+        }    
+    return 1;
+}
+
+bool mapFilled(vector<Country>& countries){
+    //iterate through set instead
+    for(size_t i=0; i<countries.size(); i++){
+        if(countries[i].color ==0){  // consider 0 to be ok
+            return 0; //if true means a country has same color as a neighbor
+        }
+    }
+    return 1;
+}
+
+
+void printADJList(vector<Country> &countries){
+    for(size_t i=0; i<countries.size(); i++){
+        cout << "Country: " << countries[i].letter << countries[i].color << " Neighbors: ";
+            for(char c : countries[i].neighbors) {
+                cout << c << countries[c-65].color << " ";
+            }    
+        cout <<endl;
+    }
+}
+
+void search(int row, vector<Country>& countries){
+    int origcolor;
+    for(int i=1; i<=4; i++){
+        origcolor = countries[row].color;
+        countries[row].color = i; //can be  adjusted for actual colors 
+        if(colorsConsistant(row, countries)){
+            if(mapFilled(countries)){ //if all rows have color filled
+                printSolution(countries);
+                cout <<endl;
+                // printADJList(countries);
+                // cout<<endl;
+                return;
+            }
+            for(char c : countries[row].neighbors){
+                if(countries[c-65].color == 0){
+                    search((int)c-65, countries);
+                }
+            } 
+        }
+        countries[row].color = origcolor;
+    }
+}
+
+bool compareAlphName(const Country &a, const Country &b)
+{
+    return a.letter < b.letter;
+}
+
+
+//explore should get all the indexes of all of the countries, and a set of all neighboring countries
+void explore(vector<vector<pair<char,int> > >&map, char letter, int x, int y, vector<Country>& countries, int iter){
+    if(x <0 || y <0 || x>=map[0].size()|| y>=map.size()){
+        return;
+    }
+    if(map[y][x].second==0 && map[y][x].first == letter){
+        map[y][x].second = 1;
+        explore(map, letter, x, y-1, countries, iter);
+        explore(map, letter, x-1, y, countries, iter);
+        explore(map, letter, x+1, y, countries, iter);
+        explore(map, letter, x, y+1, countries, iter);
+    }
+    else if(map[y][x].first != letter){
+        countries[iter].neighbors.insert(map[y][x].first); //add neighboring country's name
+    }
+}
+
+
+int main(int argc, char* argv[]){
+    if(argc < 2){
+        cerr << "Please include a .txt file with the map" << endl;
+        return 1;
+    } 
+    //the # of letters, rows and columns in the map 
+    size_t nletters; size_t colsize; size_t rowsize; string rowofletters;
+    ifstream myfile(argv[1]);
+    if(myfile.good()){
+        myfile >> nletters;
+        myfile >> colsize;
+        myfile >> rowsize;
+        vector<vector<pair<char, int> > >map(colsize, vector<pair<char, int> >(rowsize)); //stores letter and if map
+        //initialize map
+        vector<Country> countries;
+        getline(myfile,rowofletters); // need to get rid of the '/n' after the constants in line 1
+        for(size_t y=0; y<colsize; y++){   
+            getline(myfile,rowofletters);
+            for(size_t x=0; x<rowsize; x++){
+                map[y][x].first = rowofletters.at(x);
+                map[y][x].second = 0; 
+            }
+        }
+        //explore map, 
+        int iter = 0;
+        for(size_t y=0; y<colsize; y++){
+            for(size_t x=0; x<rowsize; x++){
+                if(map[y][x].second == 0){
+                    countries.push_back(Country(map[y][x].first, 0));//   countries[iter].letter = map[y][x].first; color = 0
+                    explore(map, countries[iter].letter, x, y, countries, iter); 
+                    iter++;
+                }
+            }
+        }
+        sort(countries.begin(), countries.end(), compareAlphName); //ensure 'A' is at index 0, 'B' at 1 etc
+        // printADJList(countries);
+        // cout<<endl;
+        search(0, countries);
+            // printSolution(countries);
+            // printADJList(countries);
+    }
+    return 0;
+}
---
+++ hw6/search.cpp
@@ -0,0 +1,88 @@
+#include <queue> 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+void explore(vector<vector<pair<char,int> > >&map, char letter, int x, int y, vector<int>& lcounts, int count){
+    if(x <0 || y <0 || x>=map[0].size()|| y>=map.size()){
+        return;
+    }
+    if(map[y][x].second==0 && map[y][x].first == letter){
+        map[y][x].second = 1;
+        lcounts[count]++;
+        //explore above
+        explore(map, letter, x, y-1, lcounts, count);
+        //explore left
+        explore(map, letter, x-1, y, lcounts, count);
+        //explore right
+        explore(map, letter, x+1, y, lcounts, count);
+        //explore bottom
+        explore(map, letter, x, y+1, lcounts, count);
+    }
+
+    //explore above
+
+
+        // if(y-1>0){ //if index exists
+        //     //if unexplored
+        //     if(map[y-1][x].second == 0){
+        //         //if same country
+        //         if(map[y-1][x].first == letter){
+        //             lcounts[count]++;
+        //         }
+        //     }
+            
+        // }
+    //explore left
+    //explore right
+    //explore bottom
+}
+
+int main(int argc, char* argv[]){
+	if(argc < 2){
+        cerr << "Please include a .txt file with the map" << endl;
+        return 1;
+    } 
+    //the # of letters, rows and columns in the map 
+    size_t nletters;
+    size_t colsize;
+    size_t rowsize;
+    string rowofletters;
+    int count = 0;
+    ifstream myfile(argv[1]);
+    if(myfile.good()){
+        myfile >> nletters;
+        myfile >> colsize;
+        myfile >> rowsize;
+        vector<vector<pair<char, int> > >map(colsize, vector<pair<char, int> >(rowsize)); //stores letter and if map
+        getline(myfile,rowofletters); // need to get rid of the '/n' after the constants in line 1
+        for(size_t y=0; y<colsize; y++){
+            getline(myfile,rowofletters);
+            for(size_t x=0; x<rowsize; x++){
+                map[y][x].first = rowofletters.at(x);
+                map[y][x].second = 0; 
+            }
+            //  for(size_t x=0; x<rowsize; x++){
+            //     cout <<map[y][x].first;
+            // }
+            // cout<<endl;
+        }
+        vector<int> lcounts; //not sure, what is max num of countries?
+        for(size_t y=0; y<colsize; y++){
+            for(size_t x=0; x<rowsize; x++){
+                if(map[y][x].second == 0){
+                    lcounts.push_back(0);
+                    explore(map, map[y][x].first, x, y, lcounts, count);
+                    count++;
+                }
+            }
+        }
+        cout << *max_element(lcounts.begin(), lcounts.end());
+        return 0;
+
+    }
+    return 0;
+}
